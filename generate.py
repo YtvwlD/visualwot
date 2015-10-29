cbbe1f583c7a2ca795e0e02abf4ab2a0ad77ec54
Niklas Sombert <niklas@ytvwld.de>
2015-10-29 18:40:01 +0100

    Initialize GPG.

---
+++ generate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from subprocess import Popen, PIPE
+import os
+
+class InvalidArgumentException(Exception):
+    pass
+
+class WebofTrustVisualization():
+    def __init__(self, folder, pubkey=None):
+        self.folder = folder
+        self.gpg2_dir = os.path.join(folder, "gnupg")
+        self.gpg2_commandline = ["gpg2", "--homedir", self.gpg2_dir, "--keyserver", "pgp.mit.edu"]
+        if not os.path.exists(self.folder):
+            os.mkdir(self.folder)
+        if os.path.exists(os.path.join(self.gpg2_dir, "pubring.gpg")) and not pubkey:
+            print ("Continuing...")
+        elif not os.path.exists(self.gpg2_dir) and pubkey:
+            print ("Initializing with key {}...".format(pubkey))
+            os.mkdir(self.gpg2_dir, mode=0o700)
+            self.recv_key(pubkey)
+        else:
+            raise InvalidArgumentException
+
+    def recv_key(self, pubkey):
+        cmd = list(self.gpg2_commandline)
+        cmd.append("--recv-keys")
+        cmd.append(pubkey)
+        Popen(cmd)
