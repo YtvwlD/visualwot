775d2f122a273f5210ea78ca31e25b6e7dbd97d6
Niklas Sombert <niklas@ytvwld.de>
2015-11-06 15:43:32 +0100

    Make the CLI useful.

--- cli.py
+++ cli.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
-import generate
+from generate import WebofTrustVisualization
 
 #parse the arguments
 parser = ArgumentParser()
+
+parser.add_argument("folder")
+parser.add_argument("--pubkey")
+
 args = parser.parse_args()
+
+if args.pubkey:
+	w = WebofTrustVisualization(args.folder, pubkey=args.pubkey)
+	w.gendot()
+else:
+	w = WebofTrustVisualization(args.folder)
+	w.nextRound()
+	w.gendot()
