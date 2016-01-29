e2883b3da80c8d2a3912edd076c5d042ca9201b1
Niklas Sombert <niklas@ytvwld.de>
2016-01-29 17:17:17 +0100

    Use python-gnupg.

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from subprocess import Popen, PIPE
+from gnupg import GPG
 import os
 
 class InvalidArgumentException(Exception):
@@ -9,15 +10,16 @@ class WebofTrustVisualization():
 	def __init__(self, folder, pubkey=None):
 		self.folder = folder
 		self.gpg2_dir = os.path.join(folder, "gnupg")
-		self.gpg2_commandline = ["gpg2", "--homedir", self.gpg2_dir, "--keyserver", "pgp.mit.edu"]
 		if not os.path.exists(self.folder):
 			os.mkdir(self.folder)
 		if os.path.exists(os.path.join(self.gpg2_dir, "pubring.gpg")) and not pubkey:
 			print ("Continuing...")
 			self.getNumber()
+			self.gpg2 = GPG(gpgbinary="gpg2", gnupghome=self.gpg2_dir)
 		elif not os.path.exists(self.gpg2_dir) and pubkey:
 			print ("Initializing with key {}...".format(pubkey))
 			os.mkdir(self.gpg2_dir, mode=0o700)
+			self.gpg2 = GPG(gpgbinary="gpg2", gnupghome=self.gpg2_dir)
 			self.recv_keys(pubkey)
 			self.number = 0
 			self.saveNumber()
@@ -25,17 +27,11 @@ class WebofTrustVisualization():
 			raise InvalidArgumentException
 
 	def recv_keys(self, *pubkeys):
-		cmd = list(self.gpg2_commandline)
-		cmd.append("--recv-keys")
-		for pubkey in pubkeys:
-			cmd.append(pubkey)
-		Popen(cmd).wait()
+		self.gpg2.recv_keys("pgp.mit.edu", *pubkeys)
 
 	def gendot(self):
-		cmd = list(self.gpg2_commandline)
-		cmd.append("--list-sigs")
 		with open(os.path.join(self.folder, "{}.dot".format(self.number)), "w") as dot:
-			with Popen(cmd, stdout=PIPE) as gpg2:
+			with Popen(self.gpg2.make_args(["--list-sigs"], False), stdout=PIPE) as gpg2:
 				Popen(["sig2dot", "-a"], stdin=gpg2.stdout, stdout=dot).wait()
 
 	def draw(self, format="ps"):
@@ -43,10 +39,9 @@ class WebofTrustVisualization():
 			Popen(["dot", "-T{}".format(format), "{}.dot".format(os.path.join(self.folder, str(self.number)))], stdout=drawing).wait()
 
 	def nextRound(self):
-		cmd = list(self.gpg2_commandline)
-		cmd.append("--list-sigs")
 		sigs_missing = []
-		with Popen(cmd, stdout=PIPE, env={"LANG": "C.UTF-8"}) as gpg2:
+		print ("gpg2")
+		with Popen(self.gpg2.make_args(["--list-sigs"], False), stdout=PIPE, env={"LANG": "C.UTF-8"}) as gpg2:
 			for sig in gpg2.stdout.read().splitlines():
 				if b"[User ID not found]" in sig:
 					for elem in sig.split():
